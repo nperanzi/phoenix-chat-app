82b1d9155958216024004aed282ef16795a47d51
nperanzi <nperanzi@gmail.com>
2020-01-07 17:22:51 -0500

    Fixed error with port for heroku deployment

--- config/prod.exs
+++ config/prod.exs
@@ -10,6 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :chat, ChatWeb.Endpoint,
+  http: [port: {:system, "PORT"}],
   load_from_system_env: true,
   url: [scheme: "https", host: "secure-beach-05238.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
